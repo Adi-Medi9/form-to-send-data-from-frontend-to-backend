77255a3fba66a0dfcd56f3567e7bda577e8333c5
AdiMedi-9 <ramagyaup277001.com>
2025-01-14 22:29:58 +0530

    2

--- project/project/urls.py
+++ project/project/urls.py
@@ -19,8 +19,16 @@ from django.urls import path ,include
 from vege.views import *
 from django.conf import settings
 from django.conf.urls.static import static
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('vege.urls')),
-] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT) + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+]
+
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+
+urlpatterns += staticfiles_urlpatterns()
--- project/vege/urls.py
+++ project/vege/urls.py
@@ -6,5 +6,7 @@ from . import views
 
 urlpatterns = [
     path('', views.receipes, name="home"),
+    path('delete_receipe/<id>/', views.delete_receipe, name="delete_receipe"),
+    path('update_receipe/<id>/', views.update_receipe, name="update_receipe"),
     
 ]
